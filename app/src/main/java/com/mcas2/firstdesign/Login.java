02621c116e31afd5af2d2c63e4f77c0407636a4d
Dfallax <dfalla0001@gmail.com>
2023-11-12 22:30:01 +0100

    version 1.0

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -2,6 +2,11 @@ package com.mcas2.firstdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView alert =findViewById(R.id.alertDialog);
+
+
+        ImageView background = findViewById(R.id.backgroundSplash);
+        background.setAlpha(0.9f);
+       // Glide.with(this)
+          //      .load("https://images.unsplash.com/photo-1599811392833-a39014faf967?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+           //     .transition(DrawableTransitionOptions.withCrossFade(1000))
+           //     .centerCrop()
+             //   .into(background);
     }
 }
