3b069205f0c590bda3c35f041ce130b5979d8da9
Dfallax <dfalla0001@gmail.com>
2023-11-12 23:01:31 +0100

    theme en la activity y colores en los botones.

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -18,11 +18,13 @@ public class Login extends AppCompatActivity {
 
 
         ImageView background = findViewById(R.id.backgroundSplash);
-        background.setAlpha(0.9f);
-       // Glide.with(this)
-          //      .load("https://images.unsplash.com/photo-1599811392833-a39014faf967?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-           //     .transition(DrawableTransitionOptions.withCrossFade(1000))
-           //     .centerCrop()
-             //   .into(background);
+        background.setAlpha(0.8f);
+
+       Glide.with(this)
+               .load("https://images.unsplash.com/photo-1599811392833-a39014faf967?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+               .transition(DrawableTransitionOptions.withCrossFade(500))
+               .centerCrop()
+               .into(background);
+
     }
 }
