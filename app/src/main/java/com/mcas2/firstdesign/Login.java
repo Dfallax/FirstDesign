4597ef4e5e8a2640217daf887ffbd4503861670c
Dfallax <dfalla0001@gmail.com>
2023-11-13 12:49:16 +0100

    material3

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -1,9 +1,17 @@
 package com.mcas2.firstdesign;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -15,8 +23,46 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView alert =findViewById(R.id.alertDialog);
+        alert.setPaintFlags(alert.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        alert.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        AlertDialog.Builder alerta = new AlertDialog.Builder(Login.this);
+        alerta.setMessage("¿Haz olvidado tu contraseña?")
+                .setCancelable(false)
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(Login.this, "PUTO", Toast.LENGTH_SHORT).show();
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog title = alerta.create();
+                title.show();
+    }
+});
+
+        TextView animUsername = findViewById(R.id.appNameLogin);
 
 
+        animUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Animation fadeAnimation = AnimationUtils.loadAnimation(Login.this, R.anim.translate_login);
+                animUsername.startAnimation(fadeAnimation);
+
+            }
+        });
         ImageView background = findViewById(R.id.backgroundSplash);
         background.setAlpha(0.15f);
 
