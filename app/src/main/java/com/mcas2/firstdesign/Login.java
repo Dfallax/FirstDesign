f09e590eb4ccd977a397cbd92ea020169c4799db
Dfallax <dfalla0001@gmail.com>
2023-11-13 11:26:55 +0100

    version funcional

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -18,7 +18,7 @@ public class Login extends AppCompatActivity {
 
 
         ImageView background = findViewById(R.id.backgroundSplash);
-        background.setAlpha(0.8f);
+        background.setAlpha(0.15f);
 
        Glide.with(this)
                .load("https://images.unsplash.com/photo-1599811392833-a39014faf967?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
