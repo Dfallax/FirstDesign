eaa0bc50374a533cddb3517fd3161a2301124f67
Dfallax <dfalla0001@gmail.com>
2023-12-10 23:30:07 +0100

    Uso de los fragemnts.

---
+++ app/src/main/java/com/mcas2/firstdesign/RutasFragmentos/primeraFragment.java
@@ -0,0 +1,23 @@
+package com.mcas2.firstdesign.RutasFragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mcas2.firstdesign.R;
+
+
+public class primeraFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primera, container, false);
+    }
+}
